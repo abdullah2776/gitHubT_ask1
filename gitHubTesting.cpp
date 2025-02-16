c4bdf09e95fba2977759904ba8ac28fffe300fef
DELL <hassan.owdeh@gmail.com>
2025-02-16 13:57:20 +0300

    Hasan

--- gitHubTesting.cpp
+++ gitHubTesting.cpp
@@ -4,5 +4,8 @@ int main(){
     cout<<"Hello world"<<endl;
     cout<<"first update "<<endl;
     cout<<"second update"<<endl;
+    //declaration num and print this number 
+    int num1=0;
+    cout<<num1<<endl
     return 0;
 }
